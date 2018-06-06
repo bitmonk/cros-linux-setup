af1e125c7ac256b895803a3d87e4fd650623604b
Justin Alan Ryan <bitmonk@icloud.com>
2018-06-06 11:30:23 +0000

    debian, not ubuntu, for official crostini.

--- setup.sh
+++ setup.sh
@@ -6,11 +6,12 @@ sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common \
+    gnupg \
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 
