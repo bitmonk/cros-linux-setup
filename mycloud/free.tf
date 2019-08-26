00b39219ef8b5dd7ff3fb5a16225fadc721942f8
Justin A Ryan <justin.alan.ryan@gmail.com>
2019-08-26 13:42:07 -0700

    add vpc, import state.

--- mycloud/free.tf
+++ mycloud/free.tf
@@ -3,6 +3,10 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_vpc" "AnxietySandbox2019VPC-VPC" {
+  cidr_block = "10.42.0.0/16"
+}
+
 resource "aws_instance" "stats" {
   ami           = "ami-0a313d6098716f372"
   instance_type = "t2.micro"
